8bc90a7e5b731525c5d39177ec3559caf67bb08b
Shreyas Kamate <shreyaskamate@Shreyass-MacBook-Air.local>
2026-05-06 22:11:52 +0530

    Fixed Render build script issue

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const config = {
   port: Number(process.env.PORT || 5000),
   clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://kamateshreyas_db_user:<olhTfd18KhXy5bgP>@cluster0.gqfnjxo.mongodb.net/?appName=Cluster0",
+  mongoUri: process.env.MONGO_URI || "mongodb+srv://kamateshreyas_db_user:<Admin123>@cluster0.gqfnjxo.mongodb.net/?appName=Cluster0",
   aiServiceUrl: process.env.AI_SERVICE_URL || "http://localhost:8000",
   uploadDir: "uploads"
 };
