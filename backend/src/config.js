cc79012a7165cf015989c251a2768730e08477ef
Shreyas Kamate <shreyaskamate@Shreyass-MacBook-Air.local>
2026-05-06 22:33:50 +0530

    Fixed MongoDB Atlas URI

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,8 +4,16 @@ dotenv.config();
 
 export const config = {
   port: Number(process.env.PORT || 5000),
-  clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://kamateshreyas_db_user:Admin123@cluster0.gqfnjxo.mongodb.net/?appName=Cluster0",
-  aiServiceUrl: process.env.AI_SERVICE_URL || "https://smart-pothole-management-system-1.onrender.com",
+
+  clientUrl:
+    process.env.CLIENT_URL || "http://localhost:5173",
+
+  mongoUri:
+    process.env.MONGO_URI ||
+    "mongodb+srv://kamateshreyas_db_user:Admin123@cluster0.gqfnjxo.mongodb.net/potholeDB?retryWrites=true&w=majority&appName=Cluster0",
+
+  aiServiceUrl:
+    process.env.AI_SERVICE_URL || "http://localhost:8000",
+
   uploadDir: "uploads"
 };
